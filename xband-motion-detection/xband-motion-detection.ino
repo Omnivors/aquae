b048f103c14932124eb9b5168992c1c2c3e69b0f
Giovannni Bologni <giovannibologni@gmail.com>
2019-10-13 18:20:18 +0200

    Check if motor running before detecting proximity

--- xband-motion-detection/xband-motion-detection.ino
+++ xband-motion-detection/xband-motion-detection.ino
@@ -3,21 +3,14 @@
  *      A program to process an input waveform from a Doppler motion     *
  *      sensing module, such as the HB100 or Parallax X-Band Radar       *
  *      motion detector. It uses the FreqMeasure library, which needs    *
- *      digital pin 8 or the Arduino UNO (or 49 on the MEGA)             *                                                                 *
+ *      digital pin 8 or the Arduino UNO (or 49 on the MEGA).            *
  *                                                                       *
- *      Author: B.D. Fraser                                              *
- *                                                                       *
- *        Last modified: 12/06/2017                                      *
  *                                                                       *
  *************************************************************************/
 
 // Freq Measure lib - uses digital pin 8 of Arduino Uno for measurement
 #include <FreqMeasure.h>
-
 #include <TimeLib.h>
-
-// start now time variable in seconds
-int time_now = now();
 
 void setup() {
   // initialise serial monitor for display messages
@@ -27,34 +20,65 @@ void setup() {
   FreqMeasure.begin();
 }
 
+// start now time variable in seconds
+int time_now = now();
+int time_before = 0;
+
 // global variables for calculating Doppler freq
-double total=0;
-int counter=0;
+double total = 0;
+int counter = 0;
+float dopplerFreq = 0.0;
+bool movement_detected = false;
+
 const int min_readings = 5;
 const int min_freq = 0;
 
-
+bool motor_is_running = false;
 
 void loop() {
 
   // Reset the time_now to now
   time_now = now();
-  
-  if (FreqMeasure.available()) {
-    
-    // take an average of frequency readings
-    total += FreqMeasure.read();
-    counter++;
-    // after min_readings freq readings - take average and print result
-    if (counter > min_readings) {
-      float dopplerFreq = FreqMeasure.countToFrequency(total/counter);
-      if (dopplerFreq > min_freq) {
-        Serial.print(time_now);
-        Serial.print(" frequency: ");
-        Serial.println(dopplerFreq);
-      }
-      total = 0;
-      counter = 0;
-    }
+
+  // simulate motor ON/OFF every 5 seconds. 
+  // These lines should not exist in the final sketch!
+  if (time_before - time_now > 5) {
+    motor_is_running = not motor_is_running;
+    time_before = time_now;
   }
+
+  // If the motor is not rotating, check if someone is moving with X-Band motion detector
+  if (not motor_is_running) {
+    if (FreqMeasure.available()) {
+      // take an average of frequency readings
+      total += FreqMeasure.read();
+      counter++;
+      // after min_readings freq readings - take average and print result
+      if (counter > min_readings) {
+        dopplerFreq = FreqMeasure.countToFrequency(total/counter);
+        if (dopplerFreq > min_freq) {
+          movement_detected = true;
+        }
+        else{
+          movement_detected = false;
+        }
+        Serial.print(time_now); Serial.print(" frequency: ");  Serial.println(dopplerFreq);
+        total = 0;
+        counter = 0;
+      }
+    }
+  
+    if (movement_detected) {
+      play_audio_sample();
+    }  
+  }
+
+  // the motor is running, so just wait for it to stop before checking proximity again
+  else{
+    delay(250);
+  }
+  
 }
+
+// to implement/copy from other sketches
+void play_audio_sample() {}
