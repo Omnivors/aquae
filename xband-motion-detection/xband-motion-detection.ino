881c6757e5b8b9876552be6781da739c93f7aa00
Giovannni Bologni <giovannibologni@gmail.com>
2019-10-13 18:37:17 +0200

    Launch a function when someone is detected

--- xband-motion-detection/xband-motion-detection.ino
+++ xband-motion-detection/xband-motion-detection.ino
@@ -33,23 +33,23 @@ bool movement_detected = false;
 const int min_readings = 5;
 const int min_freq = 0;
 
-bool motor_is_running = false;
-
 void loop() {
 
   // Reset the time_now to now
   time_now = now();
 
-  // simulate motor ON/OFF every 5 seconds. 
-  // These lines should not exist in the final sketch!
-  if (time_before - time_now > 5) {
-    motor_is_running = not motor_is_running;
-    time_before = time_now;
-  }
+  // check if someone is moving with X-Band motion detector
+  movement_detected = check_movements();
+  
+  if (movement_detected) {
+    Serial.print("Someone is moving! Do something");
+    play_audio_sample();
+  } 
+}
 
-  // If the motor is not rotating, check if someone is moving with X-Band motion detector
-  if (not motor_is_running) {
-    if (FreqMeasure.available()) {
+// frequency reading
+bool check_movements(){
+  if (FreqMeasure.available()) {
       // take an average of frequency readings
       total += FreqMeasure.read();
       counter++;
@@ -67,17 +67,8 @@ void loop() {
         counter = 0;
       }
     }
-  
-    if (movement_detected) {
-      play_audio_sample();
-    }  
-  }
-
-  // the motor is running, so just wait for it to stop before checking proximity again
-  else{
-    delay(250);
-  }
-  
+    
+  return movement_detected;
 }
 
 // to implement/copy from other sketches
